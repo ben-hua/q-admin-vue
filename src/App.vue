848d61513c2525d68c22de90106a3c66fbfa74b4
Benjiamin Hua <117179803+ben-hua@users.noreply.github.com>
2023-07-28 11:25:38 +0800

    Initialize site

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-view :class="[mode]" />
+  <setting-component />
+</template>
+<script setup lang="ts">
+import { computed } from 'vue';
+
+import SettingComponent from '@/layouts/setting/index.vue';
+import { useSettingStore } from '@/store';
+
+const store = useSettingStore();
+
+const mode = computed(() => {
+  return store.displayMode;
+});
+</script>
+<style lang="less" scoped>
+#nprogress .bar {
+  background: var(--td-brand-color) !important;
+}
+</style>
